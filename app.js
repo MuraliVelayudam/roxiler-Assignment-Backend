44e85428173406047260b029ab2c47875f4d4897
MuraliVelayudam <murali.velayudam@gmail.com>
2024-06-18 18:00:34 +0530

    App Update

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
 import productRouter from "./router/productRouter.js";
 
 const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/", productRouter);
 
