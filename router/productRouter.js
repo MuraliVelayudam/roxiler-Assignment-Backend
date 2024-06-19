9224cd67801f85a096673cee99ed72a4689ad159
MuraliVelayudam <murali.velayudam@gmail.com>
2024-06-19 16:57:25 +0530

    API_2 Updated - Completed

--- router/productRouter.js
+++ router/productRouter.js
@@ -12,7 +12,7 @@ productRouter.get("/", (req, res) => {
 productRouter.get("/api/transaction", async (req, res) => {
   // pagination values will be like page = 1, per page = 10
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 100;
+  const limit = Number(req.query.limit);
 
   //   Skip
   const skip = (page - 1) * limit;
@@ -119,13 +119,12 @@ productRouter.get("/api/statistics", async (req, res) => {
     });
 
     res.status(200).json({
-      statistics,
       success: true,
       message: "Fetch statistics successfully",
       sale_Statistics: Math.round(sale),
       sold: sold,
       notSold: notSold,
-      totalItems: statistics.length || null,
+      totalItems: statistics.length,
     });
   } catch (error) {
     console.error(error);
